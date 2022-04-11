e0798b4bb2b82d9a2f122400d0313a4c5986701c
BreakfastAtKyrils <kyrildr@gmail.com>
2022-04-11 15:15:45 +0000

    add 5 select queries in 1_queries

---
+++ 1_queries/1_most_visited_cities.sql
@@ -0,0 +1,6 @@
+SELECT city, count(reservations.property_id) as total_reservations
+FROM properties
+JOIN reservations 
+ON properties.id = reservations.property_id
+GROUP BY city
+ORDER BY total_reservations DESC;
